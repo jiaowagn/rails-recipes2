dc4b0cf70873a86f6ed6a3ff96a67c291f7e2bc5
jiaowagn <wangjiaohere@gmail.com>
2017-10-02 15:47:24 +0800

    add status to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,20 +1,22 @@
 class Event < ApplicationRecord
+  STATUS = ["draft", "public", "private"]
 
- validates_presence_of :name, :friendly_id
- validates_uniqueness_of :friendly_id
- validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
+  validates_presence_of :name, :friendly_id
+  validates_uniqueness_of :friendly_id
+  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
+  validates_inclusion_of :status, :in => STATUS 
 
- before_validation :generate_friendly_id, :on => :create
+  before_validation :generate_friendly_id, :on => :create
 
 
- def to_param
+  def to_param
    self.friendly_id
- end
+  end
 
- protected
+  protected
 
- def generate_friendly_id
+  def generate_friendly_id
    self.friendly_id ||= SecureRandom.uuid
- end
+  end
 
 end
