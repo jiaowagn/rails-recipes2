ab8612cd5d4fb1a86e229dbdc738dad26d114876
jiaowagn <wangjiaohere@gmail.com>
2017-10-05 20:41:26 +0800

    implement step2/update_step2/step3/update_step3 routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :events do
-    resources :registrations
+    resources :registrations do
+      member do
+        get "step/2" => "registrations#step2", :as => :step2
+        patch "step/2/update" => "registrations#step2_update", :as => :update_step2
+        get "step/3" => "registrations#step3", :as => :step3
+        patch "step/3/update" => "registrations#step3_update", :as => :update_step3
+      end
+    end
   end
 
   namespace :admin do
