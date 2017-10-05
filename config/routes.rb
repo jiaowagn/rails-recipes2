27517145aea3a6a813941ab7d79f8a22d06f0015
jiaowagn <wangjiaohere@gmail.com>
2017-10-05 20:31:54 +0800

    implement registrations#new/show function

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,16 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :events
+  resources :events do
+    resources :registrations
+  end
 
   namespace :admin do
     root "events#index"
     resources :events do
       resources :tickets, :controller => "event_tickets"
       member do
-        post :reorder 
+        post :reorder
       end
       collection do
         post :bulk_update
