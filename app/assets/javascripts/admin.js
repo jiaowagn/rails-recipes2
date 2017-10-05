0664b26ebbe8c01ca47b7120000254afe2dc7f79
jiaowagn <wangjiaohere@gmail.com>
2017-10-05 16:30:40 +0800

    add ckeditor

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -5,4 +5,5 @@
 //= require select2
 //= require nested_form_fields
 //= require bootstrap-datepicker/core
-//= require bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN 
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN
+//= require ckeditor/init
