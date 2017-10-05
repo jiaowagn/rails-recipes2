ed9d4a6e9cec567690aaebd85cc0e62c1e7acdb3
jiaowagn <wangjiaohere@gmail.com>
2017-10-05 21:32:11 +0800

    implement registration step1/step1_update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -16,6 +16,19 @@ class RegistrationsController < ApplicationController
     end
   end
 
+  def step1
+    @registration = @event.registrations.find_by_uuid(params[:id])
+  end
+
+  def step1_update
+    @registration = @event.registrations.find_by_uuid(params[:id])
+    if @registration.update(registration_params)
+      redirect_to step2_event_registration_path(@event, @registration)
+    else
+      render "step1"
+    end
+  end
+
   def step2
     @registration = @event.registrations.find_by_uuid(params[:id])
   end
