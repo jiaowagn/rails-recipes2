7a72705e516618a62ccb6b1239356f00061bd2be
jiaowagn <wangjiaohere@gmail.com>
2017-10-08 13:43:04 +0800

    add filter by registration_id

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -4,13 +4,17 @@ class Admin::EventRegistrationsController < AdminController
   def index
     @registrations = @event.registrations.includes(:ticket).order("id DESC").page(params[:page]).per(10)
 
+    if params[:registration_id].present?
+      @registrations = @registrations.where(:id => params[:registration_id].split(","))
+    end 
+
     if params[:start_on].present?
       @registrations = @registrations.where("created_at >= ?", Date.parse(params[:start_on]).beginning_of_day)
     end
 
     if params[:end_on].present?
       @registrations = @registrations.where("created_at <= ?", Date.parse(params[:end_on]).end_of_day)
-    end 
+    end
 
     if Array(params[:statuses]).any?
       @registrations = @registrations.by_status(params[:statuses])
