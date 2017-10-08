55c68acc43c4dd922872668e66334a1ab00635a2
jiaowagn <wangjiaohere@gmail.com>
2017-10-08 15:21:36 +0800

    add images to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
   mount_uploader :logo, EventLogoUploader
+  mount_uploaders :images, EventImageUploader
+  serialize :images, JSON
+
   include RankedModel
   ranks :row_order
 
