3e3d318aacac8c4c5a9334074372511c706a740b
jiaowagn <wangjiaohere@gmail.com>
2017-10-20 13:31:21 +0800

    implement is_admin? & is_editor?

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,12 @@ class User < ApplicationRecord
     self.email.split("@").first
   end
 
+  def is_admin?
+    self.role == "admin"
+  end
+
+  def is_editor?
+    ["admin", "editor"].include?(self.role)
+  end
+
 end
